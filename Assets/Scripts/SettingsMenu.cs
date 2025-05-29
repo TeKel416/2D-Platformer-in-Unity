87304c80ada5ffc1aeedf3ca33bb0b0dc17c7cb2
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2025-05-28 21:46:58 -0300

    Add AudioMixer e ajuste de audio

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Slider masterVol, musicVol, sfxVol;
+    public AudioMixer mainAudioMixer;
+
+    public void ChangeMasterVolume()
+    {
+        mainAudioMixer.SetFloat("MasterVol", masterVol.value);
+        Debug.Log(masterVol.value);
+    }
+
+    public void ChangeMusicVolume()
+    {
+        mainAudioMixer.SetFloat("MusicVol", musicVol.value);
+    }
+
+    public void ChangeSFXVolume()
+    {
+        mainAudioMixer.SetFloat("SFXVol", sfxVol.value);
+    }
+}
